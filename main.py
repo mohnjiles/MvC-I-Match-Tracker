23d4ca20d6f5e38fbe47c1b1025439d9449e5e61
JT <mohnjiles@gmail.com>
2017-12-16 16:45:33 -0800

    Removed unused method

--- main.py
+++ main.py
@@ -7,33 +7,6 @@ from imageutils import ImageUtils
 from fuzzywuzzy import fuzz
 import operator
 
-def fuzzy_substring(needle, haystack):
-    """Calculates the fuzzy match of needle in haystack,
-    using a modified version of the Levenshtein distance
-    algorithm.
-    The function is modified from the levenshtein function
-    in the bktree module by Adam Hupp"""
-    m, n = len(needle), len(haystack)
-
-    # base cases
-    if m == 1:
-        return not needle in haystack
-    if not n:
-        return m
-
-    row1 = [0] * (n+1)
-    for i in range(0,m):
-        row2 = [i+1]
-        for j in range(0,n):
-            cost = ( needle[i] != haystack[j] )
-
-            row2.append( min(row1[j+1]+1, # deletion
-                               row2[j]+1, #insertion
-                               row1[j]+cost) #substitution
-                           )
-        row1 = row2
-    return min(row1)
-
 def search_image_for_text(img, textToFind, minRatio = 75):
     ocrText = pytesseract.image_to_string(img)
     ratio = fuzz.ratio(textToFind, ocrText.rstrip().lower())
